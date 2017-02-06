f61638a6dfa7bc9ecf6398aa6b8b23b7e1115a18
hanyoupcho <hanyoup.cho@gmail.com>
2017-02-07 00:05:41 +0900

    Add component status

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({message}) => {
-	return (
-		<h2 className="Header text-center">
-			{message}
-		</h2>
-	);
-};
-
-Header.propTypes = {
-	message: React.PropTypes.string
-};
-
-const App = () => {
-	return (
-		<div className="App">
-			<Header message="Naming Contests" />
-			<div>
-				...
-			</div>
-		</div>
-	);
-};
-
-// App.propTypes = {
-// 	headerMessage: React.PropTypes.string.isRequired
-// };
-
-// App.defaultProps = {
-// 	headerMessage: 'Hello!!'
-// };
+import App from './component/App';
 
 ReactDOM.render(
 	<App />,
