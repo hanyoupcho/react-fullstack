072e77dcb44e05cbdbefc1d23e863ecc5a271950
hanyoupcho <hanyoup.cho@gmail.com>
2017-02-07 21:55:29 +0900

    Add sass

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,27 @@
 
-import React from'react';
+import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
 	state = {
 		pageHeader: 'Naming Contests'
 	};
+	componentDidMount() {
+		// ajax, timers, listners
+	}
+	componentWillUnmount() {
+		// clean ajax, tiemrs, listners
+		// avoid memory leak
+	}
 	render() {
 		return (
 			<div className="App">
 				<Header message={this.state.pageHeader} />
 				<div>
-					{this.state.test}
+					{this.props.contests.map(contest =>
+						<ContestPreview {...contest} />
+					)}
 				</div>
 			</div>
 		);
