0cb6e29d1a126ee5aa42a30962a92dbc13edc731
hanyoupcho <hanyoup.cho@gmail.com>
2017-02-08 22:45:43 +0900

    setState data injection

--- src/component/App.js
+++ src/component/App.js
@@ -1,31 +1,39 @@
-
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
-	state = {
-		pageHeader: 'Naming Contests'
-	};
-	componentDidMount() {
-		// ajax, timers, listners
-	}
-	componentWillUnmount() {
-		// clean ajax, tiemrs, listners
-		// avoid memory leak
-	}
-	render() {
-		return (
-			<div className="App">
-				<Header message={this.state.pageHeader} />
+  state = {
+    pageHeader: 'Naming Contests',
+    contests: []
+  };
+  componentDidMount() {
+    // ajax, timers, listners
+		axios.get('http://localhost:8080/api/contests')
+			.then(res => {
+				this.setState({
+					contests: res.data.contests
+				});
+			})
+			.catch(console.error);
+  }
+  componentWillUnmount() {
+    // clean ajax, tiemrs, listners
+    // avoid memory leak
+  }
+  render() {
+    return (
+			<div className = "App">
+				<Header message = {this.state.pageHeader} />
 				<div>
-					{this.props.contests.map(contest =>
-						<ContestPreview {...contest} />
+					{this.state.contests.map(contest =>
+						< ContestPreview key={contest.id} {...contest} />
 					)}
 				</div>
 			</div>
-		);
-	}
+    );
+  }
 }
 
 export default App;
--- src/component/ContestPreview.js
+++ src/component/ContestPreview.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const ContestsPreview = (contest) => (
-	<div className='ContestPreview'>
-		<div className='category-name'>
-			{contest.categoryName}
-		</div>
-		<div className='contest-name'>
-			{contest.contestName}
-		</div>
-	</div>
+  <div className='ContestPreview'>
+    <div className='category-name'>
+      {contest.categoryName}
+    </div>
+    <div className='contest-name'>
+      {contest.contestName}
+    </div>
+  </div>
 );
 
 export default ContestsPreview;
